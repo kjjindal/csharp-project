a4ce9f114dbf0656a8a170f4da06adab6a09af97
kalpit jindal <67238343+kjjindal@users.noreply.github.com>
2021-10-21 08:46:56 +0530

    csv and xml file read
    how to read csv or xml file in C# in one code

---
+++ csvxml/csvxml/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿
+namespace csvxml
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.E_Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Age = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DrivingLicence = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Language = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.F_Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.F_Age = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.F_DrivingLicence = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.F_Language = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(183, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(215, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "BROWSE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.E_Name,
+            this.Age,
+            this.DrivingLicence,
+            this.Language});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 53);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(716, 150);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // E_Name
+            // 
+            this.E_Name.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.E_Name.HeaderText = "E_Name";
+            this.E_Name.Name = "E_Name";
+            this.E_Name.ReadOnly = true;
+            // 
+            // Age
+            // 
+            this.Age.HeaderText = "Age";
+            this.Age.Name = "Age";
+            this.Age.ReadOnly = true;
+            // 
+            // DrivingLicence
+            // 
+            this.DrivingLicence.HeaderText = "DrivingLicence";
+            this.DrivingLicence.Name = "DrivingLicence";
+            this.DrivingLicence.ReadOnly = true;
+            // 
+            // Language
+            // 
+            this.Language.HeaderText = "Language";
+            this.Language.Name = "Language";
+            this.Language.ReadOnly = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 209);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(184, 20);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(215, 206);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "FETCH";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.F_Name,
+            this.F_Age,
+            this.F_DrivingLicence,
+            this.F_Language});
+            this.dataGridView2.Location = new System.Drawing.Point(13, 236);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(715, 150);
+            this.dataGridView2.TabIndex = 5;
+            // 
+            // F_Name
+            // 
+            this.F_Name.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.F_Name.HeaderText = "F_Name";
+            this.F_Name.Name = "F_Name";
+            this.F_Name.ReadOnly = true;
+            // 
+            // F_Age
+            // 
+            this.F_Age.HeaderText = "F_Age";
+            this.F_Age.Name = "F_Age";
+            this.F_Age.ReadOnly = true;
+            // 
+            // F_DrivingLicence
+            // 
+            this.F_DrivingLicence.HeaderText = "F_DrivingLicence";
+            this.F_DrivingLicence.Name = "F_DrivingLicence";
+            this.F_DrivingLicence.ReadOnly = true;
+            // 
+            // F_Language
+            // 
+            this.F_Language.HeaderText = "F_Language";
+            this.F_Language.Name = "F_Language";
+            this.F_Language.ReadOnly = true;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(608, 210);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 6;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(608, 27);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown3.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn E_Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Age;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DrivingLicence;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Language;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn F_Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn F_Age;
+        private System.Windows.Forms.DataGridViewTextBoxColumn F_DrivingLicence;
+        private System.Windows.Forms.DataGridViewTextBoxColumn F_Language;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+    }
+}
+
---
+++ csvxml/csvxml/Form1.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+using System.Data;
+using System.Xml;
+using System.Configuration;
+
+
+
+
+namespace csvxml
+{
+    public partial class Form1 : Form
+    {
+
+        public string GetConfigurationValue()
+        {
+            string connectionString;
+            var ip_address = ConfigurationManager.AppSettings["ipAddress"];
+            var database = ConfigurationManager.AppSettings["database"];
+
+            var user_id = ConfigurationManager.AppSettings["userId"];
+            var password = ConfigurationManager.AppSettings["password"];
+
+            connectionString = $@"Data Source={ip_address};Initial Catalog={database};User ID={user_id};Password={password}";
+
+            return connectionString;
+
+        }
+
+
+
+
+        public void insert_rows_in_file_details(object name,object age,object drivinglicence,object language,int refrence)
+        {
+
+            try
+            {
+                string connectionString = GetConfigurationValue();
+                string sql = "";
+                SqlConnection cnn = new SqlConnection(connectionString);
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                SqlCommand command;
+                cnn.Open();
+
+
+                sql = $"insert_row_file_details {name},{age},{drivinglicence},{language},{refrence}";
+                command = new SqlCommand(sql, cnn);
+                adapter.InsertCommand = new SqlCommand(sql, cnn);
+                adapter.InsertCommand.ExecuteNonQuery();
+                command.Dispose();
+                cnn.Close();
+
+            }
+            catch
+            {
+                MessageBox.Show("insert file doesn't match required file ", "File data error",
+    MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            finally
+            {
+             
+
+            }
+
+        }
+
+        public void insert_rows_in_file_browse(string filename)
+        {
+            try
+            {
+
+                string connectionString = GetConfigurationValue();
+                string sql = "";
+                SqlConnection cnn = new SqlConnection(connectionString);
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                SqlCommand command;
+                cnn.Open();
+
+                sql = $"insert_row_file_browse '{filename}',1";
+
+                command = new SqlCommand(sql, cnn);
+                adapter.InsertCommand = new SqlCommand(sql, cnn);
+                adapter.InsertCommand.ExecuteNonQuery();
+                command.Dispose();
+
+                cnn.Close();
+
+            }
+            catch
+            {
+                MessageBox.Show("file should be not contain special character ", "File name error",
+   MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        
+
+        }
+
+        public int find_final_row()
+
+        {
+            int refrence = 0;
+
+            try
+            {
+
+                string connectionString = GetConfigurationValue();
+                string sql = "";
+                SqlConnection cnn = new SqlConnection(connectionString);
+                SqlCommand command;
+                SqlDataReader datareader;
+                cnn.Open();
+
+                sql = "last_sno_file_browse";
+                command = new SqlCommand(sql, cnn);
+                datareader = command.ExecuteReader();
+                while (datareader.Read())
+                {
+                    refrence = datareader.GetInt32(0);
+                }
+                datareader.Close();
+                command.Dispose();
+
+            }
+            catch
+            {
+                MessageBox.Show("some thing is wrong please try again", "Last entry error",
+  MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return refrence;
+
+
+
+        }
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void xmlread(string filename,List <string> s)
+        {
+            int refrence = 0;
+            insert_rows_in_file_browse(s[s.Count - 1]);
+            refrence = find_final_row();
+            XmlReader file;
+            DataSet ds = new DataSet();
+            file = XmlReader.Create($"{filename}", new XmlReaderSettings());
+            ds.ReadXml(file);
+            int count=0;
+            for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+            {
+                //to set datagrid view data 
+                int n = dataGridView1.Rows.Add();
+                try
+                {
+                    dataGridView1.Rows[n].Cells[0].Value = ds.Tables[0].Rows[i].ItemArray[0];
+                    dataGridView1.Rows[n].Cells[1].Value = ds.Tables[0].Rows[i].ItemArray[2];
+                    dataGridView1.Rows[n].Cells[2].Value = ds.Tables[0].Rows[i].ItemArray[3];
+                    dataGridView1.Rows[n].Cells[3].Value = ds.Tables[0].Rows[i].ItemArray[1];
+                    insert_rows_in_file_details(ds.Tables[0].Rows[i].ItemArray[0], ds.Tables[0].Rows[i].ItemArray[2], ds.Tables[0].Rows[i].ItemArray[3], ds.Tables[0].Rows[i].ItemArray[1], refrence);
+                    count += 1;
+                }
+                catch
+                {
+                    MessageBox.Show("Xml File doesn't match official Format", "File format is wrong",
+ MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+                }
+                finally
+                {
+                    numericUpDown3.Value = count;
+                }
+
+
+
+            }
+
+
+
+
+
+
+        }
+
+        public void csvread(string filename, List<string> s)
+        {
+            int refrence;
+            int count = 0;
+           
+
+            using (var reader = new StreamReader($"{filename}"))
+            {
+
+                insert_rows_in_file_browse(s[s.Count - 1]);
+                refrence = find_final_row();
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+
+                    int n =dataGridView1.Rows.Add();
+
+                    try
+                    {
+                        
+                        dataGridView1.Rows[n].Cells[0].Value = !int.TryParse(values[0].Split(',')[0], out int num1) ? values[0].Split(',')[0] : null; ;
+                        dataGridView1.Rows[n].Cells[1].Value =int.Parse(values[0].Split(',')[1]);
+                        dataGridView1.Rows[n].Cells[2].Value = values[0].Split(',')[2];
+                        dataGridView1.Rows[n].Cells[3].Value = !int.TryParse(values[0].Split(',')[3],out int num2)? values[0].Split(',')[3]:null;
+                        insert_rows_in_file_details(values[0].Split(',')[0], values[0].Split(',')[1], values[0].Split(',')[2], values[0].Split(',')[3], refrence);
+                        count += 1;
+                    }
+                    catch
+                    {
+                        MessageBox.Show("CSV File doesn't match official Format", "File format is wrong",
+    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        break;
+
+
+                    }
+                    finally
+                    {
+                        numericUpDown3.Value = count;
+                    }
+                   
+
+
+                    
+                }
+
+
+
+
+
+
+
+            }
+
+
+
+
+
+            }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                dataGridView1.Rows.Clear();
+                dataGridView1.Refresh();
+                string filename = openFileDialog1.FileName;
+                List<string> s = new List<string>(filename.Split(new string[] { "\\" }, StringSplitOptions.None));
+                textBox1.Text = s[s.Count - 1];
+
+
+                if (s[s.Count - 1].Contains(".csv"))
+                {
+                    csvread(filename, s);
+
+
+                }
+                else if (s[s.Count - 1].Contains(".xml"))
+                {
+                    xmlread(filename, s);
+
+
+                }
+                else
+                {
+                    MessageBox.Show("File type should be .csv or .xml only ", "File type error",
+       MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+
+                }
+
+
+            }
+            else
+            {
+                MessageBox.Show("Please select .csv or .Xml file ", "Select file error",
+    MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+
+           
+            
+          
+
+
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView2.Rows.Clear();
+            dataGridView2.Refresh();
+            int xmlsno = (int)numericUpDown1.Value;
+            string connectionString = GetConfigurationValue();
+            SqlConnection cnn = new SqlConnection(connectionString);
+            cnn.Open();
+            string sql = "";
+            SqlCommand command;
+            SqlDataReader datareader;
+            int count = 0;
+
+            try
+            {
+                sql = $"file_no_file_details {xmlsno}";
+                command = new SqlCommand(sql, cnn);
+                datareader = command.ExecuteReader();
+              
+
+
+                if (datareader.HasRows)
+                {
+                    while (datareader.Read())
+                    {
+                        count += 1;
+                        int n = dataGridView2.Rows.Add();
+                        dataGridView2.Rows[n].Cells[0].Value = datareader.GetValue(0);
+                        dataGridView2.Rows[n].Cells[1].Value = datareader.GetInt32(1);
+                        dataGridView2.Rows[n].Cells[2].Value = datareader.GetValue(2);
+                        dataGridView2.Rows[n].Cells[3].Value = datareader.GetValue(3);
+
+                    }
+                    datareader.Close();
+                    command.Dispose();
+                    cnn.Close();
+                }
+                else
+                {
+                    MessageBox.Show("doesn't match with record", "Select file error",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+
+            }
+            catch
+            {
+
+                MessageBox.Show("something is wrong please try again", "Select file error",
+  MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            finally
+            {
+                    numericUpDown2.Value = count;
+
+            }
+
+
+
+        }
+    }
+}
