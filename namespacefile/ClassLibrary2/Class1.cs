c4477b80d3165a28dfc9ec1f2f3b6eb5c4327eca
kalpit jindal <67238343+kjjindal@users.noreply.github.com>
2021-10-21 08:56:01 +0530

    namespace and class conept
    how to add other class library in c sharp project using add refrence (class should be public )

---
+++ namespacefile/ClassLibrary2/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace ClassLibrary2
+{
+    public class Check
+    {
+        string _fname;
+        string _lname;
+        public void customer(string fname, string lname)
+        {
+            this._fname = fname;
+            this._lname = lname;
+
+        }
+        public void printname()
+        {
+            Console.WriteLine($"My name is {this._fname} {this._lname}");
+
+        }
+
+        public static void main(string[] args)
+        {
+            Check c1 = new Check();
+            c1.customer("kalpit", "jindal");
+            c1.printname();
+        }
+
+    }
+
+}
