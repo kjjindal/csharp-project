b12d3f6ef424973e4cb24012025cadfaecd06a92
kalpit jindal <67238343+kjjindal@users.noreply.github.com>
2021-10-27 09:16:59 +0530

    27-10-2021
    c sharp project

---
+++ ThreadingConcept/ThreadingConcept/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadingConcept
+{
+    class Program
+    {
+        static void print()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(1000);
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            ThreadStart th = new ThreadStart(ThreadingConcept.Program.print);
+            Thread t = new Thread(th);
+            t.Start();
+            Thread t2 = new Thread(th);
+            t2.Start();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ chequeDetails/chequeDetails/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chequeDetails
+{
+    public class Class1
+    {
+        public bool IsNumeric(string value)
+        {
+            return value.All(char.IsNumber);
+        }
+
+        public static void Main()
+        {
+            Class1 obj1 = new Class1();
+            Console.WriteLine(obj1.IsNumeric("23"));
+          
+        }
+
+        
+
+        
+    }
+}
