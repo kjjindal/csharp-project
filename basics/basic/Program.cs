c1b1d5b28c2d5599b9c5f0eda72e7b1677b66b12
kalpit jindal <67238343+kjjindal@users.noreply.github.com>
2021-10-27 09:19:04 +0530

    27-10-2021
    27-10-2021

---
+++ basics/basic/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic
+{
+    class Program
+    {
+
+        public  void evennumber()
+        {
+            Console.WriteLine("even number");
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            //18-10-2021
+            //Console.Write("Enter your name here :");
+            //string name=Console.ReadLine();
+            //Console.WriteLine(name + " jindal");
+
+            //string interploation
+            //Console.WriteLine("hello {0}", name);
+            // Console.WriteLine($"your good name is {name}");
+
+
+            //@ is used to use forward slash as a forward slash not escape slash
+            Console.WriteLine(@"C:\Users\kalpit.jindal\Desktop");
+
+            //create different method 
+            Program name = new Program();
+            name.evennumber();
+           
+
+
+
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
