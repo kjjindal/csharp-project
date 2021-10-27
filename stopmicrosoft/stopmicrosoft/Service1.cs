8eba74e4006ebeef46f90f42d28657311c471c1e
kalpit jindal <67238343+kjjindal@users.noreply.github.com>
2021-10-27 09:18:07 +0530

    27-10-2021
    27-10-2021

---
+++ stopmicrosoft/stopmicrosoft/Service1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stopmicrosoft
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            timer1.Enabled = true;
+        }
+
+        protected override void OnStop()
+        {
+            timer1.Enabled = false;
+
+        }
+
+        private void timer1_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Process[] p = Process.GetProcesses();
+            for(int i = 0; i < p.Length; i++)
+            {
+                if (p[i].ProcessName.ToUpper().Contains("MICROSOFT"))
+                {
+                    p[i].Kill();
+                    break;
+                }
+            }
+
+
+        }
+    }
+}
